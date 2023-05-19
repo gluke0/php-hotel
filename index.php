beed9230beee3da9e87cc546ea9de3d5bbdcb1cf
gluke0 <milanesigianluca@gmail.com>
2023-05-19 16:07:57 +0200

    completo

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ $hotels = [
 
     <title>PHP Hotel</title>
 </head>
-<body>
+<body class="bg-light">
 
     <div class="p-4">
         <form method="GET" class="ms-5">
@@ -107,6 +107,7 @@ $hotels = [
             </tbody>
         </table>
     </div>   
+
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>    
 </body>
